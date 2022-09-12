494021a3f5c3d52998fd03e0c362ff601a1432be
karton92 <michalaugustyn92@gmail.com>
2022-09-13 00:18:39 +0200

    Resolve merge conflict

--- src/components/App.js
+++ src/components/App.js
@@ -13,31 +13,7 @@ const App = () => {
   const urlAccounts = `https://recruitmentdb-508d.restdb.io/rest/accounts`;
   const urlAccountsTypes = `https://recruitmentdb-508d.restdb.io/rest/accounttypes`;
 
-  const fetchData = useCallback(async (url, api, setMethod) => {
-    setIsLoading(true);
-    const response = await axios({
-      method: "get",
-      headers: {
-        "x-api-key": api,
-      },
-      url: url,
-    });
-    await setMethod(response.data);
-    console.log(response.data);
-
-    setIsLoading(false);
-  }, []);
-
-  useEffect(() => {
-    fetchData(urlAccountsTypes, apiKey, setAccountsTypes);
-    fetchData(urlAccounts, apiKey, setAccounts);
-  }, []);
-  return (
-    <div className="container">
-      <h2>Zadanie IG GROUP</h2>
-      <div className="table-header"></div>
-    </div>
-  );
+  return <div className="container"></div>;
 };
 
 export default App;
